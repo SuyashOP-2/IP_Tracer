7adf2ed59c2154b28b6cc6fbd259f0c64327b235
Suyash Shirtar <104569186+SuyashOP-2@users.noreply.github.com>
2023-08-12 12:58:33 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-require('dotenv').config(); // Load environment variables from .env file
+require('dotenv').config(); 
 
 const app = express();
 const port = 3001;
